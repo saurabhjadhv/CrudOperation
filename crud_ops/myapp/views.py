71b49050eab035d31f8a9ecc008b3c80d63a2b7f
Saurabh Jadhav <108194965+saurabhjadhv@users.noreply.github.com>
2022-09-19 11:56:37 +0530

    Add files via upload

---
+++ crud_ops/myapp/views.py
@@ -0,0 +1,46 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from .forms import StudentRegistration
+from .models import User
+
+
+# Create your views here.
+
+def add_show(request):
+    if request.method == 'POST':
+        fm = StudentRegistration(request.POST)
+        if fm.is_valid():
+            nm = fm.cleaned_data['name']
+            em = fm.cleaned_data['email']
+            pw = fm.cleaned_data['password']
+            reg = User(name=nm, email=em, password=pw)
+            reg.save()
+            fm = StudentRegistration()
+    else:
+        fm = StudentRegistration()
+    stud = User.objects.all()
+    return render(request, 'myapp/add_show.html', {'form':fm, 'stu':stud})
+
+
+# This Function is Update the data.
+
+def update_data(request,id):
+    if request.method == 'POST':
+        pi = User.objects.get(pk=id)
+        fm = StudentRegistration(request.POST, instance=pi)
+        if fm.is_valid():
+            fm.save()
+    else:
+        pi = User.objects.get(pk=id)
+        fm = StudentRegistration(instance=pi)
+    return render(request,'myapp/update_data.html', {'form':fm})
+
+
+
+# This Function to delete the data.
+
+def delete_data(request, id):
+    if request.method == 'POST':
+        pi = User.objects.get(pk=id)
+        pi.delete()
+        return HttpResponseRedirect('/')
